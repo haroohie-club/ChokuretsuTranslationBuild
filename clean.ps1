fbce7ed041c8b1b7ec40c2f6ef746ee5fc7dae5b
Ermelber <ermelber@live.it>
2022-02-23 10:05:09 +0100

    Add clean script and update setup script

---
+++ clean.ps1
@@ -0,0 +1,2 @@
+Remove-Item -Path rom -Recurse -Force -Confirm:$false
+Remove-Item -Path original -Recurse -Force -Confirm:$false
--- setup.ps1
+++ setup.ps1
@@ -4,12 +4,15 @@ Param(
 
 $nitroPackerArgs = @("-u", "original.nds", "rom", "HaruhiChokuretsu")
 & $nitroPacker $nitroPackerArgs
-Push-Location archives
+
 mkdir original
+Push-Location original
+mkdir archives
 Pop-Location
-Copy-Item -Path "rom/data/dat.bin" -Destination "archives/original/dat.bin"
-Copy-Item -Path "rom/data/evt.bin" -Destination "archives/original/evt.bin"
-Copy-Item -Path "rom/data/grp.bin" -Destination "archives/original/grp.bin"
-Copy-Item -Path "rom/data/scn.bin" -Destination "archives/original/scn.bin"
+
+Copy-Item -Path "rom/data/dat.bin" -Destination "original/archives/dat.bin"
+Copy-Item -Path "rom/data/evt.bin" -Destination "original/archives/evt.bin"
+Copy-Item -Path "rom/data/grp.bin" -Destination "original/archives/grp.bin"
+Copy-Item -Path "rom/data/scn.bin" -Destination "original/archives/scn.bin"
 Copy-Item -Path "rom/arm9.bin" -Destination "asm/arm9.bin"
-Copy-Item -Recurse -Path "rom/overlay/" -Destination "overlays/"
+Copy-Item -Recurse -Path "rom/overlay/" -Destination "original/"
