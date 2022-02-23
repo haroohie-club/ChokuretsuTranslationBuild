35d9288290c114a5a4bad419bf778877119eafed
Ermelber <ermelber@live.it>
2022-02-23 13:51:10 +0100

    Fix scripts with correct env vars and edit asm folder name

--- build.ps1
+++ build.ps1
@@ -1,19 +1,13 @@
 Param(
   [string]$nitroPacker = "NitroPacker",
-  [string]$haruhiCli = "HaruhiChokuretsuCLI"
+  [string]$haruhiCli = $env:CHOKURETSU_CLI_PATH
 )
 
-Push-Location asm
-mkdir rom
 & "./build.exe" asm
-Copy-Item -Path "rom/arm9.bin" -Destination "../rom/arm9.bin"
-Remove-Item -Path "rom/arm9.bin"
-Remove-Item -Path rom -Recurse -Force
-Pop-Location
 
 Copy-Item -Path "rominfo.xml" -Destination "rom/HaruhiChokuretsu.xml"
 
-$haruhiCliArgs = @("-p", "-i", "overlay.xml", "-f", "overlays", "-o", "rom/overlay", "--rom-info", "rom/HaruhiChokuretsu.xml")
+$haruhiCliArgs = @("-p", "-i", "overlay.xml", "-f", "original/overlay", "-o", "rom/overlay", "--rom-info", "rom/HaruhiChokuretsu.xml")
 & "$haruhiCli"  $haruhiCliArgs
 
 $nitroPackerArgs = @("-p", "rom/HaruhiChokuretsu.xml", "HaruhiChokuretsu.nds")
--- build_arc.ps1
+++ build_arc.ps1
@@ -1,17 +1,18 @@
 Param(
-  [string]$haruhiCli = "HaruhiChokuretsuCLI",
-  [string]$evtResxFolder,
-  [string]$datResxFolder,
+  [string]$haruhiCli = $env:CHOKURETSU_CLI_PATH,
+  [string]$evtResxFolder = "$env:CHOKURETSU_STRINGS_ROOT\strings",
+  [string]$datResxFolder = "$env:CHOKURETSU_STRINGS_ROOT\strings_dat",
+  [string]$archiveFolder = $env:CHOKURETSU_ASSETS_ROOT,
   [string]$resxLangCode = "en",
-  [string]$fontMap = "font_replacement.json"
+  [string]$fontMap = "$env:CHOKURETSU_ASSETS_ROOT\misc\charset.json"
 )
 
-$datResxArgs = @("--import-resx", "-i", "archives\original\dat.bin", "-o", "rom\data\dat.bin", "-f", $datResxFolder, "-l", $resxLangCode, "--font-map", $fontMap)
-$datArgs = @("-r", "-i", "rom\data\dat.bin", "-o", "rom\data\dat.bin", "-f", "archives\data")
-$evtResxArgs = @("--import-resx", "-i", "archives\original\evt.bin", "-o", "rom\data\evt.bin", "-f", $evtResxFolder, "-l", $resxLangCode, "--font-map", $fontMap)
-$grpArgs = @("-r", "-i", "archives\original\grp.bin", "-o", "rom\data\grp.bin", "-f", "archives\graphics")
-$grpLocArgs = @("-r", "-i", "rom\data\grp.bin", "-o", "rom\data\grp.bin", "-f", "archives\graphics_loc\$resxLangCode")
-$scnArgs = @("-r", "-i", "archives\original\scn.bin", "-o", "rom\data\scn.bin", "-f", "archives\scn")
+$datResxArgs = @("--import-resx", "-i", "original\archives\dat.bin", "-o", "rom\data\dat.bin", "-f", $datResxFolder, "-l", $resxLangCode, "--font-map", $fontMap)
+$datArgs = @("-r", "-i", "rom\data\dat.bin", "-o", "rom\data\dat.bin", "-f", "$archiveFolder\data")
+$evtResxArgs = @("--import-resx", "-i", "original\archives\evt.bin", "-o", "rom\data\evt.bin", "-f", $evtResxFolder, "-l", $resxLangCode, "--font-map", $fontMap)
+$grpArgs = @("-r", "-i", "original\archives\grp.bin", "-o", "rom\data\grp.bin", "-f", "$archiveFolder\graphics\shared")
+$grpLocArgs = @("-r", "-i", "rom\data\grp.bin", "-o", "rom\data\grp.bin", "-f", "$archiveFolder\graphics\$resxLangCode")
+$scnArgs = @("-r", "-i", "original\archives\scn.bin", "-o", "rom\data\scn.bin", "-f", "$archiveFolder\scn")
 
 & $haruhiCli $datResxArgs
 & $haruhiCli $datArgs
