bbc7d10ecad112ea50f566686436d0d9e22adfc7
jonko0493 <jonko0493@protonmail.com>
2022-03-01 06:58:25 -0800

    Adjust scripts to match refactored CLI

--- build.ps1
+++ build.ps1
@@ -7,7 +7,7 @@ Param(
 
 Copy-Item -Path "rominfo.xml" -Destination "rom/HaruhiChokuretsu.xml"
 
-$haruhiCliArgs = @("-p", "-i", "overlay.xml", "-f", "original/overlay", "-o", "rom/overlay", "--rom-info", "rom/HaruhiChokuretsu.xml")
+$haruhiCliArgs = @("patch-overlays", "-i", "original/overlay", "-o", "rom/overlay", "-p", "overlay.xml", "-r", "rom/HaruhiChokuretsu.xml")
 & "$haruhiCli"  $haruhiCliArgs
 
 $nitroPackerArgs = @("-p", "rom/HaruhiChokuretsu.xml", "HaruhiChokuretsu.nds")
--- build_arc.ps1
+++ build_arc.ps1
@@ -5,12 +5,12 @@ Param(
   [string]$resxLangCode = "en"
 )
 
-$datResxArgs = @("--import-resx", "-i", "original\archives\dat.bin", "-o", "rom\data\dat.bin", "-f", "$stringsFolder\strings_dat", "-l", $resxLangCode, "--font-map", "$archiveFolder\misc\charset.json")
-$datArgs = @("-r", "-i", "rom\data\dat.bin", "-o", "rom\data\dat.bin", "-f", "$archiveFolder\data")
-$evtResxArgs = @("--import-resx", "-i", "original\archives\evt.bin", "-o", "rom\data\evt.bin", "-f", "$stringsFolder\strings", "-l", $resxLangCode, "--font-map", "$archiveFolder\misc\charset.json")
-$grpArgs = @("-r", "-i", "original\archives\grp.bin", "-o", "rom\data\grp.bin", "-f", "$archiveFolder\graphics\shared")
-$grpLocArgs = @("-r", "-i", "rom\data\grp.bin", "-o", "rom\data\grp.bin", "-f", "$archiveFolder\graphics\$resxLangCode")
-$scnArgs = @("-r", "-i", "original\archives\scn.bin", "-o", "rom\data\scn.bin", "-f", "$archiveFolder\scn")
+$datResxArgs = @("import-resx", "-i", "original\archives\dat.bin", "-o", "rom\data\dat.bin", "-r", "$stringsFolder\strings_dat", "-l", $resxLangCode, "-f", "$archiveFolder\misc\charset.json")
+$datArgs = @("replace", "-i", "rom\data\dat.bin", "-o", "rom\data\dat.bin", "-r", "$archiveFolder\data")
+$evtResxArgs = @("import-resx", "-i", "original\archives\evt.bin", "-o", "rom\data\evt.bin", "-r", "$stringsFolder\strings", "-l", $resxLangCode, "-f", "$archiveFolder\misc\charset.json")
+$grpArgs = @("replace", "-i", "original\archives\grp.bin", "-o", "rom\data\grp.bin", "-r", "$archiveFolder\graphics\shared")
+$grpLocArgs = @("replace", "-i", "rom\data\grp.bin", "-o", "rom\data\grp.bin", "-r", "$archiveFolder\graphics\$resxLangCode")
+$scnArgs = @("replace", "-i", "original\archives\scn.bin", "-o", "rom\data\scn.bin", "-r", "$archiveFolder\scn")
 
 & $haruhiCli $datResxArgs
 & $haruhiCli $datArgs
