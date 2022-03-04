9ba07fd5290b79eccf3cdfdf3cb7e3600306c938
jonko0493 <jonko0493@protonmail.com>
2022-03-04 10:19:12 -0800

    Add splash screen versioning

--- build_arc.ps1
+++ build_arc.ps1
@@ -2,9 +2,11 @@ Param(
   [string]$haruhiCli = $env:CHOKURETSU_CLI_PATH,
   [string]$stringsFolder = "$env:CHOKURETSU_STRINGS_ROOT",
   [string]$archiveFolder = $env:CHOKURETSU_ASSETS_ROOT,
-  [string]$resxLangCode = "en"
+  [string]$resxLangCode = "en",
+  [string]$version
 )
 
+$splashScreenArgs = @("version-screen", "-v", "$version", "-s", "$archiveFolder\graphics\$resxLangCode\splash\splash_screen.png", "-f", "$archiveFolder\graphics\$resxLangCode\splash\Nunito-Black.ttf", "-o", "$archiveFolder\graphics\$resxLangCode\8b7_newpal_tidx0_splash_screen.png")
 $datResxArgs = @("import-resx", "-i", "original\archives\dat.bin", "-o", "rom\data\dat.bin", "-r", "$stringsFolder\strings_dat", "-l", $resxLangCode, "-f", "$archiveFolder\misc\charset.json")
 $datArgs = @("replace", "-i", "rom\data\dat.bin", "-o", "rom\data\dat.bin", "-r", "$archiveFolder\data")
 $evtResxArgs = @("import-resx", "-i", "original\archives\evt.bin", "-o", "rom\data\evt.bin", "-r", "$stringsFolder\strings", "-l", $resxLangCode, "-f", "$archiveFolder\misc\charset.json")
@@ -12,6 +14,11 @@ $grpArgs = @("replace", "-i", "original\archives\grp.bin", "-o", "rom\data\grp.b
 $grpLocArgs = @("replace", "-i", "rom\data\grp.bin", "-o", "rom\data\grp.bin", "-r", "$archiveFolder\graphics\$resxLangCode")
 $scnArgs = @("replace", "-i", "original\archives\scn.bin", "-o", "rom\data\scn.bin", "-r", "$archiveFolder\scn")
 
+& $haruhiCli $splashScreenArgs
+if ($LASTEXITCODE -ne 0) {
+  Write-Error "HaruhiChokuretsuCLI failed on string replacement in dat.bin with exit code $LASTEXITCODE."
+  exit 1
+}
 & $haruhiCli $datResxArgs
 if ($LASTEXITCODE -ne 0) {
   Write-Error "HaruhiChokuretsuCLI failed on string replacement in dat.bin with exit code $LASTEXITCODE."
@@ -42,3 +49,4 @@ if ($LASTEXITCODE -ne 0) {
   Write-Error "HaruhiChokuretsuCLI failed on file replacement in scn.bin with exit code $LASTEXITCODE."
   exit 1
 }
+Remove-Item -Path "$archiveFolder\graphics\$resxLangCode\8b7_newpal_tidx0_splash_screen.png"
