603992c61ed00e9f2cf8f61203a0c4eaa36a10d3
jonko0493 <jonko0493@protonmail.com>
2022-03-11 20:23:58 -0800

    Fix speech bubble bug (in overlays!!)

---
+++ src/overlays/main_0006.s
@@ -0,0 +1,14 @@
+# Overlay file reference
+
+# Fixes bug with Kyon's speech bubbles during group selection not rendering long text
+# actual in-place replacement, not branch-link function
+arepl_020C9F00:
+    ldr r0, [pc, #0x81C]
+
+# actual in-place replacement, not branch-link function
+arepl_020C9FB8:
+    ldr r0, [pc, #0x804]
+
+# actual in-place replacement, not branch-link function
+arepl_020C9FD4:
+    ldr r0, [pc, #0x7E8]
