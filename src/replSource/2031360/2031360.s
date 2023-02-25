e1773f56568da7aa10be621d9ce4b8aa5fdfd712
jonko0493 <jonko0493@protonmail.com>
2023-02-24 21:09:08 -0800

    Fix backlog bug

---
+++ src/replSource/2031360/2031360.s
@@ -0,0 +1,3 @@
+@ increase offset between backlog text and the backlog entries
+arepl_2031360:
+    add r0, r0, #0x3600
---
+++ src/replSource/2031444/2031444.s
@@ -0,0 +1,3 @@
+@ double hard limit on backlog bytes
+arepl_2031444:
+    cmp r0, #0x3600
