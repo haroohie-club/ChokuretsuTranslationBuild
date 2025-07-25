dd5834dc165718382a9097f1e255d17da1384915
jonko0493 <jonko0493@protonmail.com>
2025-07-25 00:15:17 -0700

    Make run.sh work better

--- docker/run.sh
+++ docker/run.sh
@@ -1,9 +1,9 @@
 # /bin/bash
 
 docker run --rm \
-    -v $1:/build \
-    -v $2:/assets \
-    -v $3:/strings \
+    -v $1:/build:Z \
+    -v $2:/assets:Z \
+    -v $3:/strings:Z \
     -e LANG=$4 \
     -e ROM_VERSION=$5 \
     ghcr.io/haroohie-club/chokuretsu-devkitarm:main \
